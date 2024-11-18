5ff240cb0c44d387799a832ed2890276e1d7c88a
Marek <marek@techfellow.it>
2024-11-18 11:07:57 +0100

    update k8s

--- k8s/provisionk8smaster.sh
+++ k8s/provisionk8smaster.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 mkdir -p /etc/apt/keyrings && touch /etc/apt/sources.list.d/kubernetes.list 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /" > /etc/apt/sources.list.d/kubernetes.list 
 
@@ -17,7 +19,9 @@ kubeadm config images pull
 kubeadm init --pod-network-cidr=172.20.0.0/16 --apiserver-advertise-address=192.168.63.2 --node-name=k8smaster
 
 mkdir -p /home/vagrant/.kube
-cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+mkdir -p /root/.kube
+cp -r /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+cp -r /etc/kubernetes/admin.conf /root/.kube/config
 chown vagrant:vagrant /home/vagrant/.kube/config
 
 openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //' > /vagrant/ca_cert_hash
@@ -27,11 +31,28 @@ kubeadm token list -o yaml | grep token: | awk '{print $2}' > /vagrant/kubeadm_j
 # chmod 755 /etc/kubernetes/admin.conf
 
 # Install Calico
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.2/manifests/tigera-operator.yaml
+curl -fs https://raw.githubusercontent.com/projectcalico/calico/v3.28.2/manifests/tigera-operator.yaml > /vagrant/tigera.yaml
+kubectl create -f /vagrant/tigera.yaml
 curl https://raw.githubusercontent.com/projectcalico/calico/v3.28.2/manifests/custom-resources.yaml > /vagrant/calico.yaml
 # Insert pod network CIDR in calico.yaml
 sed -i 's|cidr:.*|cidr: 172.20.0.0/16|g' /vagrant/calico.yaml
 kubectl create -f /vagrant/calico.yaml
+
+# # Install MetalLB
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.8/config/manifests/metallb-native.yaml
+# kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+# # Install Helm
+curl -fsSL -o /vagrant/get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 
+chmod 700 /vagrant/get_helm.sh
+sh /vagrant/get_helm.sh
+
+# # Install Helm Chart
+helm repo add stable https://charts.helm.sh/stable
+helm repo update
+# helm install nginx-ingress stable/nginx-ingress
+
+rm -rf /root/.kube
 
 #Sample Deployment
 # cat << EOFnginx > /vagrant/deployment.yaml
@@ -57,17 +78,3 @@ kubectl create -f /vagrant/calico.yaml
 #         - containerPort: 80
 # EOFnginx
 # kubectl apply -f /vagrant/deployment.yaml
-
-# # Install Helm
-# curl -fsSL -o get_helm.sh  https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 
-# chmod 700 get_helm.sh
-# ./get_helm.sh
-
-# # Install Helm Chart
-# helm repo add stable https://charts.helm.sh/stable
-# helm repo update
-# helm install nginx-ingress stable/nginx-ingress
-
-# # Install MetalLB
-# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.8/config/manifests/metallb-native.yaml
-# kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
