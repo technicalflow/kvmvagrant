8c96731088b422415fe0c0c2cb017fa95ee4ef54
Marek <marek@techfellow.it>
2026-03-09 13:44:35 +0100

    updating docker swarm to debian 13

--- dockerswarm/provisiondockerapt.sh
+++ dockerswarm/provisiondockerapt.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Check required software
-apt-get install -y -qq apt-transport-https ca-certificates curl
+
+curl -fsSL "https://download.docker.com/linux/ubuntu/gpg" | gpg --dearmor -o /usr/share/keyrings/docker-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-keyring.gpg] https://download.docker.com/linux/ubuntu jammy stable" > /etc/apt/sources.list.d/docker.list
 
 # Add Docker’s official GPG key
-curl -fsSL "https://download.docker.com/linux/ubuntu/gpg" | apt-key add -qq
+#curl -fsSL "https://download.docker.com/linux/ubuntu/gpg" | apt-key add -qq
 
 # Set up the stable repo
 #add-apt-repository \
@@ -11,17 +12,16 @@ curl -fsSL "https://download.docker.com/linux/ubuntu/gpg" | apt-key add -qq
 #  $(lsb_release -cs) \
 #  stable"
 
-echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" >  /etc/apt/sources.list.d/docker.list
+# echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu jammy stable" >  /etc/apt/sources.list.d/docker.list
 
 # Update the packages
 apt-get update
 
 # Install docker-ce
-apt-get install --no-install-recommends -y docker-ce
-apt-get install -y docker-compose
+apt-get install --no-install-recommends -y docker-ce docker-compose
 
 # Access docker w/o sudo
 usermod -aG docker vagrant
-systemctl restart docker
-service docker restart
+systemctl restart docker.service
+# service docker restart
 docker version
--- dockerswarm/provisionfull.sh
+++ dockerswarm/provisionfull.sh
@@ -18,10 +18,9 @@ timedatectl set-timezone Europe/Warsaw
 apt-get install -y \
     gcc \
     make \
-    perl \
     curl \
     gnupg \
-    ntp \
+    wget \
     ca-certificates \
     apt-transport-https
 
@@ -31,30 +30,38 @@ apt-get autoremove
 apt-get purge
 apt-get clean
 
-# apt-get remove virtualbox-guest-utils -y
-# sh /vagrant/VBoxLinuxAdditions.run
+if [ $(hostname) == "dsm" ] ; then sed -i '/address 192.168.50..*/a \      gateway 192.168.50.250' /etc/network/interfaces && systemctl restart networking.service; fi
 
 ip a | grep inet
+ip r
 echo DONE
 
-# Only if communicating from LAN network with VM
-cat << EOFroute > /vagrant/50-vagrant.yaml
----
-network:
-  version: 2
-  renderer: networkd
-  ethernets:
-    eth1:
-      addresses:
-      - 192.168.50.232/24
-      routes:
-      - to: 0.0.0.0/0
-        via: 192.168.50.250
-        metric: 100
-        on-link: true
-    eth2:
-      addresses:
-      - 192.168.60.2/24
-EOFroute
+# For Ubuntu
+# if [ $(hostname) == "dsm" ] ; then mv -f /tmp/50-vagrant.yaml /etc/netplan/50-vagrant.yaml && netplan apply; fi
 
-if [ $(hostname) == "dsm" ] ; then mv -f /vagrant/50-vagrant.yaml /etc/netplan/50-vagrant.yaml && netplan apply; fi
+# Only if communicating from LAN network with VM
+# if [ "$(hostname)" = "dsm" ]; then
+#   cat <<EOFroute >/tmp/50-vagrant.yaml
+# ---
+# network:
+#   version: 2
+#   renderer: networkd
+#   ethernets:
+#     eth1:
+#       addresses:
+#       - 192.168.50.232/24
+#       routes:
+#       - to: 0.0.0.0/0
+#         via: 192.168.50.250
+#         metric: 100
+#         on-link: true
+#     eth2:
+#       addresses:
+#       - 192.168.60.2/24
+# EOFroute
+# fi
+
+# if [ "$(hostname)" = "dsm" ]; then
+#   mv -f /tmp/50-vagrant.yaml /etc/netplan/50-vagrant.yaml
+#   netplan apply
+# fi
