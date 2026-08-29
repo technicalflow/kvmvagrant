e0163c4a9bb44e51799f2b156d8150be491470b7
Marek <marek@techfellow.it>
2026-08-29 18:13:37 +0200

    small updates

--- microk8s/provision.sh
+++ microk8s/provision.sh
@@ -35,6 +35,8 @@ systemctl stop swap.target
 systemctl disable swap.target
 
 modprobe br_netfilter
+modprobe overlay
+
 sysctl net.bridge.bridge-nf-call-ip6tables=1
 sysctl net.bridge.bridge-nf-call-iptables=1
 sysctl net.ipv4.ip_forward=1
